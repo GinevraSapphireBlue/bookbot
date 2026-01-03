b4da798c9e40fde9930949bfa3b6106fc2d2818f
GinevraSapphireBlue <anna.ol3ksiak@gmail.com>
2026-01-03 21:03:51 +0100

    Print a report about book contents - number of words and characters

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-from stats import count_words, count_all_chars
+from stats import count_words, count_all_chars, get_ordered_char_freq_list
 
 def get_books_text(path_to_file):
     with open(path_to_file) as book_file:
         return book_file.read()
 
-def main():
-    # print(get_books_text('./books/frankenstein.txt'))
-    frankenstein_book_text = get_books_text('./books/frankenstein.txt')
-    num_words_frankenstein = count_words(frankenstein_book_text)
-    print(f"Found {num_words_frankenstein} total words")
+def print_report(path_to_file, num_words, char_frequency):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char_freq in char_frequency:
+        char, freq = char_freq.values()
+        if char.isalpha():
+            print(f"{char}: {freq}")
+    print("============= END ===============")
 
+
+def main():
+    path_to_file = 'books/frankenstein.txt'
+    frankenstein_book_text = get_books_text(path_to_file)
+
+    # Count words in the book
+    num_words_frankenstein = count_words(frankenstein_book_text)
+    # print(f"Found {num_words_frankenstein} total words")
+
+    # Count repetitions of characters in book
     dict_of_words_frankenstein = count_all_chars(frankenstein_book_text)
-    print(dict_of_words_frankenstein)
+    # print(dict_of_words_frankenstein)
+
+    # Create ordered list of characters' frequency
+    ordered_char_frequency = get_ordered_char_freq_list(dict_of_words_frankenstein)
+    # print(ordered_char_frequency)
+
+    # Print report about the book
+    print_report(path_to_file, num_words_frankenstein, ordered_char_frequency)
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,10 @@ def count_all_chars(book_text):
         else:
             dict_chars[char_lower] = 1
     return dict_chars
+
+def get_ordered_char_freq_list(dict_chars):
+    char_freq_list = []
+    for char in dict_chars:
+        char_freq_list.append({"char": char, "num": dict_chars[char]})
+    char_freq_list.sort(reverse=True, key=lambda items: items["num"])
+    return char_freq_list
